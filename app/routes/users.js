0fd9322f0d94dfdd3cd0532537a82e97dee416eb
cozy-code <cozy.code@outlook.com>
2017-02-13 11:34:53 +0900

    access db from app.
    http://localhost:4000/users/dbtest1

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/dbtest1', function(req, res, next) {
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+    host     : 'db',
+    user     : 'root',
+    database : 'mysql',
+    password : process.env.MYSQL_ROOT_PASSWORD || 'example',
+  });
+
+  connection.connect();
+
+  connection.query('select * from user', function(err, rows, fields) {
+    if (err) throw err;
+    res.send(rows);
+  });
+
+  connection.end();
+});
+
 module.exports = router;

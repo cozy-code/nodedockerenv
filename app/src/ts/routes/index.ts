c24a878ce7a2a60ec2ce4e10827c189c62023b22
cozy-code <cozy.code@outlook.com>
2017-02-15 15:00:10 +0900

    basic route by TypeScript

---
+++ app/src/ts/routes/index.ts
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+
+let router = Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express TS' });
+});
+
+export default router;
